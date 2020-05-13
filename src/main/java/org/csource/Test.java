f34423393c98cf486a53e3f4418d942a4f433856
rc_larry <1355893275@qq.com>
2020-05-14 00:15:08 +0800

    int

--- src/main/java/org/csource/Test.java
+++ src/main/java/org/csource/Test.java
@@ -3,6 +3,7 @@ package org.csource;
 public class Test {
     public void test11(){
         System.out.println();
+        int i = 1;
     }
     public void test3(){
         System.out.println();
